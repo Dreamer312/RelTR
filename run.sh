cc2b37dc54db59ce9754bd89b58e7533d795ea30
Dreamer312 <537716499@qq.com>
2023-09-22 15:40:06 +1000

    uts-9.22-First training wan cheng

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
+# python -m torch.distributed.launch  \
+#     --nproc_per_node=2 \
+#     --use_env main.py \
+#     --dataset vg \
+#     --img_folder data/vg/images/ \
+#     --ann_path data/vg/ \
+#     --batch_size=8 \
+#     --output_dir checkpoint \
+
+
 python -m torch.distributed.launch  \
     --nproc_per_node=2 \
     --use_env main.py \
     --dataset vg \
     --img_folder data/vg/images/ \
     --ann_path data/vg/ \
-    --batch_size=7 \
-    --output_dir checkpoint \
+    --batch_size=8 \
+    --output_dir checkpoint2 \
---
+++ run_eval.sh
@@ -0,0 +1,6 @@
+python main.py \
+    --dataset vg \
+    --img_folder data/vg/images/ \
+    --ann_path data/vg/ \
+    --eval --batch_size 1 \
+    --resume checkpoint/checkpoint0149.pth
